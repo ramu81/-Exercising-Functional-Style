c0873f8c08664741bea82f4ad5e2e3f0ef396f53
Ram ( Ramudu Kondapalli) <ramudu.kp@gmail.com>
2018-10-11 08:17:54 -0400

    Add files via upload

---
+++ src/edu/chinna/kadira/Exercise6.java
@@ -0,0 +1,41 @@
+package edu.chinna.kadira;
+
+import static java.util.Comparator.comparing;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Exercise6 {
+
+	public static void main(String[] args) {
+
+		List<Person> lPersons = Arrays.asList(new Person("Ram", 1), new Person("Vasantha", 2), new Person("Sai", 3),
+				new Person("Laxmamma", 4));
+
+		List<TraditionalPerson> lTraditionalPersons = Arrays.asList(new TraditionalPerson("Ram", 1),
+				new TraditionalPerson("Vasantha", 2), new TraditionalPerson("Sai", 3),
+				new TraditionalPerson("Laxmamma", 4));
+
+		Collections.sort(lTraditionalPersons);
+		Comparator<Person> ByName = new Comparator<Person>() {
+			public int compare(Person a, Person b) {
+				return a.getName().compareTo(b.getName());
+			}
+		};
+		Collections.sort(lPersons, ByName);
+		System.out.println();
+		for (TraditionalPerson traditionalPerson : lTraditionalPersons) {
+			System.out.println(traditionalPerson);
+		}
+		System.out.println();
+		for (Person lPerson : lPersons) {
+			System.out.println(lPerson);
+		}
+		System.out.println();
+		lPersons.stream()
+				.sorted(comparing(Person::getName))
+				.forEach(System.out::println);
+	}
+}
