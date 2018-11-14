40c3ac173669b6775c3e9c52eaa3c894d00b6645
Ram ( Ramudu Kondapalli) <ramudu.kp@gmail.com>
2018-11-14 08:11:46 -0500

    Add files via upload

---
+++ src/edu/chinna/kadira/ExecuteAroundMethodPattern.java
@@ -0,0 +1,39 @@
+package ed.demo;
+
+interface UseResource {
+    void accept(Resource r);
+}
+
+class Resource {
+    private Resource() {
+        System.out.println(" Creating");
+    }
+
+    public static void use(UseResource uscResource) {
+        Resource resource = new Resource();
+        try {
+            uscResource.accept(resource);
+        } finally {
+            resource.close();
+        }
+    }
+
+    protected void op1() {
+        System.out.println(" operation...");
+    }
+
+    private void close() {
+        System.out.println(" closed...");
+    }
+}
+
+public class ExecuteAroundMethodPattern {
+    public static void main(String[] args) {
+        Resource.use(new UseResource() {
+            @Override
+            public void accept(Resource r) {
+                r.op1();
+            }
+        });
+    }
+}
