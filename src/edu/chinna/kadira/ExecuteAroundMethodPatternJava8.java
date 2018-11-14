7dc9fca0da76535fc75bb9983af40812130f75fa
Ram ( Ramudu Kondapalli) <ramudu.kp@gmail.com>
2018-11-14 08:23:43 -0500

    Add files via upload

---
+++ src/edu/chinna/kadira/ExecuteAroundMethodPatternJava8.java
@@ -0,0 +1,33 @@
+package ed.demo;
+
+
+import java.util.function.Consumer;
+
+class Resource {
+    private Resource() {
+        System.out.println(" Creating");
+    }
+
+    public static void use(Consumer<Resource> uscResource) {
+        Resource resource = new Resource();
+        try {
+            uscResource.accept(resource);
+        } finally {
+            resource.close();
+        }
+    }
+
+    protected void op1() {
+        System.out.println(" operation...");
+    }
+
+    private void close() {
+        System.out.println(" closed...");
+    }
+}
+
+public class ExecuteAroundMethodPatternJava8 {
+    public static void main(String[] args) {
+        Resource.use(Resource::op1);
+    }
+}
